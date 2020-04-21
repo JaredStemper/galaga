f45db313ac7f6a25c08db606dddf922349b7db69
Kunal Pendse <Kunal@Kunals-MacBook-Pro.local>
2020-04-21 21:35:11 +0530

    added code to screen.c

---
+++ driver/src/screen.c
@@ -0,0 +1,28 @@
+/*
+screen.c:
+	Background (all black.. Add blues later), level (bottom right), lives at the bottom left, player ship, bullets, Enemies
+Gets info for level, lives, player ship, and enemy ships from struct.c
+
+*/
+#include <f3d_lcd_sd.h>
+
+
+void drawBackground(){
+  f3d_fillScreen2(BLACK);
+  //display level
+
+  //display lives
+  
+}
+
+void drawPlayer(struct player p){
+  
+}
+
+//function to draw single enemy or array of enemies?
+void drawEnemies(){
+}
+
+void drawBullet(int x, int y, uint16_t color){
+  drawBar_vertical(x,y,y+3,color);
+}
